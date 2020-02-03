ad80067c76a10f2f338648483bb26f049192eab3
Brian Paintsil <brnpaintsil@gmail.com>
2020-02-03 16:20:17 +0000

    add test

--- tests/bricklane_platform/models/test_payment.py
+++ tests/bricklane_platform/models/test_payment.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import datetime
 
-from bricklane_platform.models.payment import Payment
+from bricklane_platform.models.payment import BankPayment, Payment
 from bricklane_platform.models.card import Card
 
 
@@ -62,3 +62,32 @@ class TestPayment(unittest.TestCase):
         payment.card = card
 
         self.assertFalse(payment.is_successful())
+
+
+class TestBankPayment(unittest.TestCase):
+
+    def test_init(self):
+        payment = BankPayment()
+
+        self.assertIsNone(payment.customer_id)
+        self.assertIsNone(payment.date)
+        self.assertIsNone(payment.amount)
+        self.assertIsNone(payment.fee)
+        self.assertIsNone(payment.bank_account_id)
+
+    def test_init_with_data(self):
+
+        data = {
+            "amount": "2000",
+            "bank_account_id": "45",
+            "customer_id": "123",
+            "date": "2019-02-01",
+        }
+
+        payment = BankPayment(data)
+
+        self.assertEqual(payment.customer_id, 123)
+        self.assertEqual(payment.date, datetime(2019, 2, 1))
+        self.assertEqual(payment.amount, 1960)
+        self.assertEqual(payment.fee, 40)
+        self.assertEqual(payment.bank_account_id, 45)
