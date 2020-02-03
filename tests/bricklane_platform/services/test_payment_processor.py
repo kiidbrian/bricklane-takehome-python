897d32dce88c17dd915eb39a52062e8a74f51510
Brian Paintsil <brnpaintsil@gmail.com>
2020-02-03 18:10:13 +0000

    add tests for BankTransferProcessor

--- tests/bricklane_platform/services/test_payment_processor.py
+++ tests/bricklane_platform/services/test_payment_processor.py
@@ -2,7 +2,7 @@ import unittest
 from ..fixture import get_path
 
 
-from bricklane_platform.services.payment_processor import PaymentProcessor
+from bricklane_platform.services.payment_processor import BankTranferProcessor, PaymentProcessor
 from bricklane_platform.models.payment import Payment
 
 
@@ -45,3 +45,29 @@ class TestPaymentProcessor(unittest.TestCase):
 
         result = self.payment_processor.verify_payments([payment1, payment2, payment3])
         self.assertEqual(result, [payment1, payment3])
+
+
+class TestBankTransferProcessor(unittest.TestCase):
+
+    def setUp(self):
+        self.payment_processor = BankTranferProcessor()
+
+    def test_get_payments(self):
+        fixture = get_path("bank_payments_data.csv")
+
+        payments = self.payment_processor.get_payments(fixture, "bank")
+        self.assertEqual(len(payments), 2)
+        self.assertEqual(payments[0].bank_account_id, 20)
+        self.assertEqual(payments[1].bank_account_id, 60)
+
+    def test_get_payments_empty(self):
+        fixture = get_path("bank_payments_empty.csv")
+
+        payments = self.payment_processor.get_payments(fixture, "bank")
+        self.assertEqual(len(payments), 0)
+
+    def test_verify_payments(self):
+        fixture = get_path("bank_payments_data.csv")
+
+        payments = self.payment_processor.get_payments(fixture, "bank")
+        self.assertEqual(len(payments), len(self.payment_processor.verify_payments(payments)))
