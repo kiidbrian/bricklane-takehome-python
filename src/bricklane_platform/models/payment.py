a28c2b750a4190ae344bfedd85cd1ef5cad1c442
Brian Paintsil <brnpaintsil@gmail.com>
2020-02-03 19:22:55 +0000

    reimplemented code

--- src/bricklane_platform/models/payment.py
+++ src/bricklane_platform/models/payment.py
@@ -1,21 +1,19 @@
 from decimal import Decimal
 from dateutil.parser import parse
 
-
 from bricklane_platform.models.card import Card
 from bricklane_platform.config import PAYMENT_FEE_RATE
 
-class BasePayment(object):
-    
+class PaymentBase(object):
     customer_id = None
     date = None
     amount = None
     fee = None
 
     def __init__(self, data=None):
-        
+
         if not data:
-            raise ValueError('Provide data')
+            return
 
         self.customer_id = int(data["customer_id"])
         self.date = parse(data["date"])
@@ -25,9 +23,10 @@ class BasePayment(object):
         self.amount = total_amount - self.fee
 
     def is_successful(self):
-        raise NotImplementedError('Implementation for success state NOT IMPLEMENTED')
+        raise NotImplementedError('Method not implemented')
 
-class Payment(BasePayment):
+
+class Payment(PaymentBase):
 
     card_id = None
 
@@ -35,24 +34,26 @@ class Payment(BasePayment):
 
         super(Payment, self).__init__(data)
 
-        card = Card()
-        card.card_id = int(data["card_id"])
-        card.status = data["card_status"]
-        self.card = card
+        if data:
+            card = Card()
+            card.card_id = int(data["card_id"])
+            card.status = data["card_status"]
+            self.card = card
 
     def is_successful(self):
         return self.card.status == "processed"
 
 
-class BankPayment(BasePayment):
+class BankPayment(PaymentBase):
 
     bank_account_id = None
 
     def __init__(self, data=None):
 
         super(BankPayment, self).__init__(data)
-        
-        self.bank_account_id = int(data["bank_account_id"])
+
+        if data:
+            self.bank_account_id = int(data['bank_account_id'])
 
     def is_successful(self):
         return True
--- src/bricklane_platform/services/payment_processor.py
+++ src/bricklane_platform/services/payment_processor.py
@@ -2,22 +2,37 @@ import csv
 
 from bricklane_platform.models.payment import BankPayment, Payment
 
-class BaseProcessor(object):
-    def get_payments(self, csv_path, source):
-        pass
 
-    def verify_payments(self):
-        pass
+class PaymentFactory(object):
 
-    
+    def __init__(self):
+        self._payment_types = {}
+
+    def register_payment(self, source, payment_type):
+        self._payment_types[source] = payment_type
+
+    def get_payment(self, source):
+        payment = self._payment_types.get(source)
+
+        if not payment:
+            raise KeyError('{} is not registered'.format(source))
+
+        return payment
+
+
 class PaymentProcessor(object):
+
+    def __init__(self):
+        self.factory = PaymentFactory()
+        self.factory.register_payment('card', Payment)
+        self.factory.register_payment('bank', BankPayment)
 
     def get_payments(self, csv_path, source):
         payments = []
         with open(csv_path) as f:
             reader = csv.DictReader(f)
             for row in reader:
-                payments.append(Payment(row))
+                payments.append(self.factory.get_payment(source)(row))
 
         return payments
 
@@ -28,17 +43,3 @@ class PaymentProcessor(object):
                 successful_payments.append(payment)
 
         return successful_payments
-
-class BankTranferProcessor(PaymentProcessor):
-
-    def get_payments(self, csv_path, source):
-        payments = []
-        with open(csv_path) as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                payments.append(BankPayment(row))
-
-        return payments
-
-    def verify_payments(self, payments):
-        return payments
